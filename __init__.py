5f8dcafb1308545aa9b66be3d79f4d0ed83fda43
SonderXiaoming <98363578+SonderXiaoming@users.noreply.github.com>
2022-08-23 18:30:41 -0500

    api变动修复，同时更新角色

--- __init__.py
+++ __init__.py
@@ -1,17 +1,30 @@
 import os
+from pyexpat import model
 from nonebot import MessageSegment
 from hoshino import Service, priv, R
 from . import getvoiece
 from .youdaotranslate import translate
 
 sv_help = '''
-【柚子中配】+ （角色id） + 文本
-【柚子日配】+ （角色id） + 文本
-不填默认为宁宁，加号省略
-【柚子id列表】查看支持角色
+【柚子/常轨脱离/缘之空】【中配/日配】+ （角色id） + 文本
+（中配将中文翻译为片假名，日配翻译为日语，日配会输出翻译内容供检查）
+【柚子/常轨脱离/缘之空】【纯日配】+ （角色id） + 文本
+（给日语N114514水平的人准备，不翻译，但要求用户直接输入日语）
+不填默id认为0，加号替换为空格
+例：柚子日配 0 我爱宁宁 
+柚子中配 我爱宁宁 
+柚子纯日配 私は綾地寧々を愛している（应该是这样，日语不好）
+【角色id列表】查看支持角色和分类
 '''.strip()
 
+speaker_dict = {
+'柚子':["綾地寧々","因幡めぐる","朝武芳乃","常陸茉子","ムラサメ","鞍馬小春","在原七海","四季ナツメ","明月栞那","墨染希","火打谷愛衣","汐山涼音"],
+'常轨脱离':["和泉妃愛","常盤華乃","錦あすみ","鎌倉詩桜","竜閑天梨","和泉里","新川広夢","聖莉々子"],
+'缘之空':["春日野穹","天女目瑛","依媛奈緒","渚一葉"],
+}
+
 speaker_id = '''
+=====柚子=====
 0；綾地寧々
 1；因幡めぐる
 2；朝武芳乃
@@ -19,7 +32,42 @@ speaker_id = '''
 4；ムラサメ
 5；鞍馬小春
 6；在原七海
+7；"四季ナツメ"
+8；"明月栞那"
+9；"墨染希"
+10；"火打谷愛衣"
+11；"汐山涼音"
+====常轨脱离====
+0；"和泉妃愛"
+1；"常盤華乃"
+2；"錦あすみ"
+3；"鎌倉詩桜"
+4；"竜閑天梨"
+5；"和泉里"
+6；"新川広夢"
+7；"聖莉々子"
+====缘之空=====
+0；"春日野穹"
+1；"天女目瑛"
+2；"依媛奈緒"
+3；"渚一葉"
 '''.strip()
+
+voice_send = R.get('record', 'demo.wav') 
+path_wav = os.path.abspath(voice_send.path)
+
+def get_speakers(choose,num=0):
+    if choose == '常轨脱离':
+        speakers,model = speaker_dict["常轨脱离"][num],1
+    elif choose == '缘之空':
+        speakers,model = speaker_dict["缘之空"][num],3
+    else:
+        speakers = speaker_dict["柚子"][num]
+        if num>=7:
+            model = 2
+        else:
+            model = 0
+    return speakers,model
 
 path = os.path.abspath(R.get('record').path)
 if not os.path.exists(path):
@@ -34,18 +82,18 @@ sv = Service(
     help_ = sv_help
 )
 
-@sv.on_prefix("柚子中配")
+@sv.on_prefix("柚子中配",'常轨脱离中配','缘之空中配')
 async def youzi_voice_cn(bot, ev):
-    text = ev.message.extract_plain_text()
+    text = ev.raw_message
     content = text.split()
-    voice_send = R.get('record', 'demo.wav') 
-    path_wav = os.path.abspath(voice_send.path)
-    if len(content) == 1:
-        text_chjp = await getvoiece.chinese2katakana(content[0])
-        A = getvoiece.getvoice()
-    elif content[0].isdigit():
+    if len(content) == 2:
+        speaker,model = get_speakers(content[0][:-2])
         text_chjp = await getvoiece.chinese2katakana(content[1])
-        A = getvoiece.getvoice(int(content[0]))
+        A = getvoiece.getvoice(speaker,model)
+    elif content[1].isdigit():
+        speaker,model = get_speakers(content[0][:-2],int(content[1]))
+        text_chjp = await getvoiece.chinese2katakana(content[2])
+        A = getvoiece.getvoice(speaker,model)
     else:
         await bot.send(ev, sv_help)
         return
@@ -54,18 +102,18 @@ async def youzi_voice_cn(bot, ev):
     final_send = MessageSegment.record(f'file:///{os.path.abspath(voice_send.path)}')
     await bot.send(ev, final_send)
 
-@sv.on_prefix("柚子日配")
+@sv.on_prefix("柚子日配",'常轨脱离日配','缘之空日配')
 async def youzi_voice_ja(bot, ev):
-    text = ev.message.extract_plain_text()
+    text = ev.raw_message
     content = text.split()
-    voice_send = R.get('record', 'demo.wav') 
-    path_wav = os.path.abspath(voice_send.path)
-    if len(content) == 1:
-        text_chjp = await translate(content[0])
-        A = getvoiece.getvoice()
-    elif content[0].isdigit():
+    if len(content) == 2:
+        speaker,model = get_speakers(content[0][:-2])
         text_chjp = await translate(content[1])
-        A = getvoiece.getvoice(int(content[0]))
+        A = getvoiece.getvoice(speaker,model)
+    elif content[1].isdigit():
+        speaker,model = get_speakers(content[0][:-2],int(content[1]))
+        text_chjp = await translate(content[2])
+        A = getvoiece.getvoice(speaker,model)
     else:
         await bot.send(ev, sv_help)
         return
@@ -75,6 +123,26 @@ async def youzi_voice_ja(bot, ev):
     final_send = MessageSegment.record(f'file:///{os.path.abspath(voice_send.path)}')
     await bot.send(ev, final_send)
 
-@sv.on_fullmatch("柚子id列表")
+@sv.on_prefix("柚子纯日配",'常轨脱离纯日配','缘之空纯日配')
+async def youzi_focus_ja(bot, ev):
+    text = ev.raw_message
+    content = text.split()
+    if len(content) == 2:
+        speaker,model = get_speakers(content[0][:-2])
+        text_chjp = content[1]
+        A = getvoiece.getvoice(speaker,model)
+    elif content[1].isdigit():
+        speaker,model = get_speakers(content[0][:-2],int(content[1]))
+        text_chjp = content[2]
+        A = getvoiece.getvoice(speaker,model)
+    else:
+        await bot.send(ev, sv_help)
+        return
+    await A.gethash(text_chjp)
+    await A.getvoice(path_wav)
+    final_send = MessageSegment.record(f'file:///{os.path.abspath(voice_send.path)}')
+    await bot.send(ev, final_send)
+
+@sv.on_fullmatch("角色id列表")
 async def speaker_list(bot, ev):
     await bot.send(ev, speaker_id)
--- getvoiece.py
+++ getvoiece.py
@@ -5,9 +5,6 @@ from lxml import etree
 import base64
 import aiohttp
 import wave
-import json
-
-speaker_list = ["model0/綾地寧々","model0/因幡めぐる","model0/朝武芳乃","model0/常陸茉子","model0/ムラサメ","model0/鞍馬小春","model0/在原七海"]
 
 async def local_hash():
     alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789'
@@ -18,8 +15,6 @@ async def local_hash():
     return a
 
 async def chinese2katakana(text):
-    #text = parse.quote(text)
-    #print(text)
     cookies = {
         '__gads': 'ID=0913d7b7838088a9-22d4a86186d500c8:T=1660228904:RT=1660228904:S=ALNI_MYLAxIRws8hObfvoeF5wkg6F8_1qg',
         '__gpi': 'UID=00000880c2f1ffa9:T=1660228904:RT=1660228904:S=ALNI_ManV7rXnEUgMAuUxsLEFkSYonxQRQ',
@@ -56,7 +51,6 @@ async def chinese2katakana(text):
     }
 
     response = requests.post('https://www.ltool.net/chinese-simplified-and-traditional-characters-pinyin-to-katakana-converter-in-simplified-chinese.php', cookies=cookies, headers=headers, data=data)
-    #print(response.text)
     html = etree.HTML(response.text)
     text = html.xpath("/html//form/div[5]/div/text()")
     text_full = ""
@@ -65,13 +59,11 @@ async def chinese2katakana(text):
     return text_full
 
 class getvoice(object):
-    def __init__(self,speaker_id=0) :
+    def __init__(self,speaker,num) :
         self.headers = {
             'authority': 'hf.space',
             'accept': '*/*',
             'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-            # Already added when you pass json=
-            # 'content-type': 'application/json',
             'origin': 'https://hf.space',
             'referer': 'https://hf.space/embed/skytnt/moe-japanese-tts/+?__theme=light',
             'sec-ch-ua': '"Chromium";v="104", " Not A;Brand";v="99", "Microsoft Edge";v="104"',
@@ -82,13 +74,14 @@ class getvoice(object):
             'sec-fetch-site': 'same-origin',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.81 Safari/537.36 Edg/104.0.1293.47',
         }
-        self.speaker = speaker_list[speaker_id]
+        self.speaker = speaker
+        self.num = num
         self.hash = local_hash()
         
     async def gethash(self,text):
         self.text = text
         self.json_data = {
-            'fn_index': 0,
+            'fn_index': self.num,
             'data': [
                 f'{text}',
                 f'{self.speaker}',
@@ -98,26 +91,22 @@ class getvoice(object):
         }
         async with aiohttp.ClientSession() as session: 
             async with session.post('https://hf.space/embed/skytnt/moe-japanese-tts/api/queue/push/', headers=self.headers, json=self.json_data) as resp:
-                #self.voicehash = await resp.json()["hash"]
                 a = await resp.json()
                 self.voicehash = a.get("hash")
-        #print(self.voicehash)
         
     async def getvoice(self,path):
         self.path = path
         self.json_data_2 = {
             'hash': f'{self.voicehash}',
         }
-        
+
         async with aiohttp.ClientSession() as session: 
             async with session.post('https://hf.space/embed/skytnt/moe-japanese-tts/api/queue/status/', headers=self.headers,json=self.json_data_2) as resp:
                 a =await resp.json()
-                #print(a)  #######注释
         if a.get('status') == 'PENDING' or a.get('status') == 'QUEUED':
             await getvoice.getvoice(self,self.path)
             return
         if a.get("data").get("data")[1] == None :
-            print(114514)  ######
             await getvoice.mixit(self,self.text)
             return
         voice_base64 = a.get("data").get("data")[1].split(",")[1]
@@ -148,17 +137,4 @@ class getvoice(object):
         output.writeframes(data[0][1])
         output.writeframes(data[1][1])
         output.close()
-            
-        
-        
-        
-        
-if __name__ == "__main__":
-    text = "车子千万不要外借！我真是吃了大亏了！朋友找我借车，碍于面子不好意思不借，结果昨天在路上遇见了，一点都不爱惜我的车，上坡还站起来蹬车，链子都掉了！气死我了！幸好今天是肯德基疯狂星期四，有人请我吃我感觉会好点"
-    path = "demo.wav"
-    text_chjp = asyncio.run(chinese2katakana(text))
-    print(text_chjp)
-    A = getvoice()
-    A.gethash(text_chjp)
-    A.getvoice(path)
     
